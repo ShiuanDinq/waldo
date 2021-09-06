ca1b35c7640632f75d0a7bedf898f52e80928611
edmund-hsd <edmundhang@ymail.com>
2021-09-06 15:16:23 +0700

    fix redirect to leaderboard after score submission

--- app/javascript/components/Game.jsx
+++ app/javascript/components/Game.jsx
@@ -1,20 +1,16 @@
 import React from "react"
 import Dialog from "./Dialog";
 import Target from "./Target";
-import { useState , useEffect } from 'react'
+import { useState , useEffect, useRef } from 'react'
 import Character from "./Character";
 import ScoreForm from "./ScoreForm";
-import { useParams } from "react-router-dom";
-import { Redirect } from 'react-router-dom'
-
-
-
+import { useParams, useHistory} from "react-router-dom";
 
 
 const Game = () => {
-  const [coords, setCoords] = useState([''])
+  const [coords, setCoords] = useState([])
   const [pic, setPic] = useState([])
-  const [characters, setCharacters] = useState('')
+  const [characters, setCharacters] = useState(null)
   const [selected, setSelected] = useState('')
   const [target, setTarget] = useState('')
   const [dialog, setDialog] = useState('')
@@ -25,23 +21,17 @@ const Game = () => {
   const [display, setDisplay] = useState('')
   const [name, setName] = useState('')
   const [start, setStart] = useState('')
+  let history = useHistory();
 
 
 
-  const tickUp = () => {
-    let time = timer + 1
-    setTimer(time)
-  }
+//Start game---------------------------------------------------------------------------
+const handleStart = () => {
+  setStart(true)
+}
 
-  const formatTimer = () => {
-    let sec = timer%60
-    let min = ~~(timer/60)
-    let fsec = `${sec<10? "0":""}${sec}` 
-    let fmin = `${min<10? "0":""}${min}`
-    let stime = `${fmin}:${fsec}`
-    setDisplay(stime)
-  }
 
+// Set game background and character data---------------------------------------------
   const getImageUrl =  async () => {
     const url = `/api/v1/games/${id}`;
     const response = await fetch(url);
@@ -56,127 +46,25 @@ const Game = () => {
     return coordFloats
   }
 
-  const showDialog = () => {
-    setDialog(true)
+  useEffect(() => {
+    getImageUrl()  
+  },[])
+
+
+// Set timer and format timer----------------------------------------------------------------
+  const tickUp = () => {
+    let time = timer + 1
+    setTimer(time)
   }
 
-  const hideDialog = () => {
-    setDialog(false)
+  const formatTimer = () => {
+    let sec = timer%60
+    let min = ~~(timer/60)
+    let fsec = `${sec<10? "0":""}${sec}` 
+    let fmin = `${min<10? "0":""}${min}`
+    let stime = `${fmin}:${fsec}`
+    setDisplay(stime)
   }
-
-  const handleCoords = (click) => {
-    setCoords([click.pageX, click.pageY])
-    console.log('setting coord')
-    setPic(click.target.getBoundingClientRect())
-  }
-
-  const handleStatus = () => {
-    if(target.name == selected){
-      setCharacters((prevState) => {  
-        const newChar = prevState.map((char) => {
-          if(char.name == selected){
-            return({name:char.name, coords:char.coords, status:true, image_url:char.image_url})
-          }else{
-            return(char)
-          }
-        })
-        return(newChar)
-      })     
-    }  
-  }
-
-  const handleSelected = (name) => {
-    setSelected(name)
-    hideDialog()
-  }
-
-  const concede = () => {
-    const picture = document.getElementById("pic")
-    setPic(picture.getBoundingClientRect())
-    setCharacters((prevState) => {  
-      const newChar = prevState.map((char) => {
-          return({name:char.name, coords:char.coords, status:true, image_url:char.image_url})
-      }) 
-      return(newChar)
-    })  
-  }
-
-  const handleTarget = () => {
-    setTarget('')
-    if(characters){
-      characters.map((character) => targetPresent(character))
-    }
-  }
-
-  const targetPresent = (character) => {
-     var character_left = (pic.width/character.coords[0])+pic.left
-     var character_right = (pic.width/character.coords[1])+pic.left
-     var character_top = (pic.height/character.coords[2])+pic.top
-     var character_bottom = (pic.height/character.coords[3])+pic.top
-
-    if(
-      coords[0] > character_left &&
-      coords[0] < character_right &&
-      coords[1] > character_top &&
-      coords[1] < character_bottom
-    ){
-      setTarget(character)
-    }
-  }
-  
-  const winning = () => {
-    if(characters){
-      return (characters.every(char => char.status==true))
-    }
-  }
-
-  const handleWin = () => {
-    if(winning()){
-      setWin(true)
-    } 
-  }
-
-  const handleName = (e) => {
-    setName(e.target.value)
-  }
-
-  const handleStart = () => {
-    setStart(true)
-  }
-
-
-  const sendWinner = async () => {
-    let apiUrl = `/api/v1/games/${id}/players`
- 
-
-    const token = document.querySelector('meta[name="csrf-token"]').content;
-    let resp = await fetch(apiUrl, {
-      method: "POST",
-      headers: {
-        "X-CSRF-TOKEN": token,
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify({ name: name, score: timer, game_id: id}),
-    });
-    // resp = await resp.json();
-    // console.log(resp)
-    // context.router.history.push(`/games/28/players`)
-    // setWinnersList(resp);
-    // document.getElementById("winner").value = "";
-
-
-  };
-
-//   .then((response) => {
-//     if (response.ok) {
-//       goHome(`/leaderboard/${pic.id}`);
-//       return response.json();
-//     }
-//     throw new Error("Network response was not ok.");
-//   })
-//   .catch((error) => console.log(error.message));
-// }
-// };
 
   useEffect(() => {
     let int;
@@ -189,6 +77,16 @@ const Game = () => {
 
   })
 
+//Set coordinates of clicks-------------------------------------------------------------------
+const handleCoords = (click) => {
+  setCoords([click.pageX, click.pageY])
+  // console.log('setting coord')
+  setPic(click.target.getBoundingClientRect())
+
+}
+
+//Check if target character is selected
+
   useEffect(() => {
     if(start){
       showDialog()
@@ -196,6 +94,34 @@ const Game = () => {
     }
 
   },[coords]) 
+
+  const handleTarget = () => {
+    setTarget('')
+    if(characters){
+      characters.map((character) => targetPresent(character))
+    }
+  }
+
+  const handleSelected = (name) => {
+    setSelected(name)
+    hideDialog()
+  }
+
+  const targetPresent = (character) => {
+    var character_left = (pic.width/character.coords[0])+pic.left
+    var character_right = (pic.width/character.coords[1])+pic.left
+    var character_top = (pic.height/character.coords[2])+pic.top
+    var character_bottom = (pic.height/character.coords[3])+pic.top
+
+   if(
+     coords[0] > character_left &&
+     coords[0] < character_right &&
+     coords[1] > character_top &&
+     coords[1] < character_bottom
+   ){
+     setTarget(character)
+   }
+ }
 
   useEffect(() => {
     handleStatus()
@@ -205,13 +131,112 @@ const Game = () => {
     setSelected('')
   },[coords]) 
 
+ // Set character status
+ const handleStatus = () => {
+  if(target.name == selected){
+    setCharacters((prevState) => {  
+      const newChar = prevState.map((char) => {
+        if(char.name == selected){
+          return({name:char.name, coords:char.coords, status:true, image_url:char.image_url})
+        }else{
+          return(char)
+        }
+      })
+      return(newChar)
+    })     
+  }  
+}
+
+
+
+//Set dialog visibility----------------------------------------------------------------------
+
+  const showDialog = () => {
+    setDialog(true)
+  }
+
+  const hideDialog = () => {
+    setDialog(false)
+  }
+
+
+
+
+
+  // const concede = () => {
+  //   const picture = document.getElementById("pic")
+  //   setPic(picture.getBoundingClientRect())
+  //   setCharacters((prevState) => {  
+  //     const newChar = prevState.map((char) => {
+  //         return({name:char.name, coords:char.coords, status:true, image_url:char.image_url})
+  //     }) 
+  //     return(newChar)
+  //   })  
+  // }
+
+
+// Set winner----------------------------------------------------------------------------------
+const sendWinner = async (e) => {
+  e.preventDefault()
+  let apiUrl = `/api/v1/games/${id}/players`
+
+  const token = document.querySelector('meta[name="csrf-token"]').content;
+  let resp = await fetch(apiUrl, {
+    method: "POST",
+    headers: {
+      "X-CSRF-TOKEN": token,
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({ name: name, score: timer, game_id: id}),
+  });
+  if(resp.ok){
+    history.push(`/games/${id}/players`)
+  }else{
+    console.log('no')
+  }
+
+
+
+
+
+};
+
+
+  const winning = () => {
+    if(characters){
+      return (characters.every(char => char.status==true))
+    }
+  }
+
+  const handleWin = () => {
+    if(winning()){
+      setWin(true)
+    } 
+  }
+
   useEffect(() => {
     handleWin()
   },[characters]) 
 
-  useEffect(() => {
-    getImageUrl()
-  },[])
+  const handleName = (e) => {
+    setName(e.target.value)
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   if(characters){
     var drawTarget = characters.map((character) => <Character pic={pic} character={character} />)
@@ -233,7 +258,7 @@ const Game = () => {
           START
         </p>
       </button>
-      <ScoreForm win={win} name={name} handleName={handleName} sendWinner={sendWinner}/>
+      <ScoreForm win={win} name={name} handleName={handleName} sendWinner={sendWinner} />
       <div className="column is-11 is-marginless is-paddingless">
         <img id="pic" className="pic" src={image} onClick={handleCoords} style={{minWidth:"100%"}}/>
       </div>
